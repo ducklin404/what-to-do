f7a11e152148c8357dd5baf70d06ad02ad8946a9
ducklin404 <duonght404@gmail.com>
2025-05-31 10:08:44 +0700

    change js inside the html

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('getActivityBtn').addEventListener('click', async () => {
-  const res = await fetch('/api');
-  const data = await res.json();
-  const mainText = document.getElementById('main-text');
-
-
-  mainText.textContent = JSON.stringify(data, null, 2);
-});
